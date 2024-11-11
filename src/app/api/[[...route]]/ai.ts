4f007aeefd9c3be4307e1f301136b519fd3449b5
王杰 <jie985563349@outlook.com>
2024-11-11 10:25:47 +0800

    add ai remove background

--- src/app/api/[[...route]]/ai.ts
+++ src/app/api/[[...route]]/ai.ts
@@ -8,28 +8,46 @@ const replicate = new Replicate({
   useFileOutput: false,
 });
 
-const app = new Hono().post('/generate-image', zValidator('json', z.object({ prompt: z.string() })), async (c) => {
-  const { prompt } = c.req.valid('json');
+const app = new Hono()
+  .post('/generate-image', zValidator('json', z.object({ prompt: z.string() })), async (c) => {
+    const { prompt } = c.req.valid('json');
 
-  const input = {
-    cfg: 3.5,
-    steps: 28,
-    prompt,
-    aspect_ratio: '3:2',
-    output_format: 'webp',
-    output_quality: 90,
-    negative_prompt: '',
-    prompt_strength: 0.85,
-  };
+    const input = {
+      cfg: 3.5,
+      steps: 28,
+      prompt,
+      aspect_ratio: '3:2',
+      output_format: 'webp',
+      output_quality: 90,
+      negative_prompt: '',
+      prompt_strength: 0.85,
+    };
 
-  const output = await replicate.run('stability-ai/stable-diffusion-3', { input });
-  const url = (output as string[])[0];
+    const output = await replicate.run('stability-ai/stable-diffusion-3', { input });
+    const url = (output as string[])[0];
 
-  if (!url) {
-    return c.json({ error: 'Something went wrong' }, 400);
-  }
+    if (!url) {
+      return c.json({ error: 'Something went wrong' }, 400);
+    }
 
-  return c.json({ data: url });
-});
+    return c.json({ data: url });
+  })
+  .post('/remove-background', zValidator('json', z.object({ image: z.string() })), async (c) => {
+    const { image } = c.req.valid('json');
+
+    const input = {
+      image,
+    };
+
+    const output: unknown = await replicate.run('cjwbw/rembg:fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003', {
+      input,
+    });
+
+    if (!output) {
+      return c.json({ error: 'Something went wrong' }, 400);
+    }
+
+    return c.json({ data: output as string });
+  });
 
 export default app;
--- src/app/design/[slug]/components/remove-background-sidebar.tsx
+++ src/app/design/[slug]/components/remove-background-sidebar.tsx
@@ -2,22 +2,59 @@
 
 import { useState } from 'react';
 import Image from 'next/image';
-import { AlertTriangleIcon } from 'lucide-react';
+import { useMutation } from '@tanstack/react-query';
+import { fabric } from 'fabric';
+import { AlertTriangleIcon, Loader2 } from 'lucide-react';
 
+import { client } from '@/lib/hono';
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import Drawer from '@/components/drawer';
 import { useEditorStore } from '../providers/editor-store';
+import { useEditorController } from '../providers/editor-controller';
 import useOnSelectionChange from '../hooks/use-on-selection-change';
 
 const RemoveBackgroundSidebar: React.FC = () => {
   const activeTool = useEditorStore((state) => state.activeTool);
   const setActiveTool = useEditorStore((state) => state.setActiveTool);
 
+  const { stage, getWorkspace, add } = useEditorController();
+
   const [selectedObject, setSelectedObject] = useState<fabric.Object>();
 
   // @ts-ignore
   const src = selectedObject?._originalElement?.currentSrc;
+
+  const addImage = (url: string) => {
+    if (!stage) return;
+
+    const callback = (image: fabric.Image) => {
+      const workspace = getWorkspace();
+
+      image.scaleToWidth(workspace?.width ?? 0);
+      image.scaleToHeight(workspace?.height ?? 0);
+
+      add(image);
+    };
+
+    fabric.Image.fromURL(url, callback, { crossOrigin: 'anonymous' });
+  };
+
+  const mutation = useMutation({
+    mutationFn: async (image: string) => {
+      const response = await client.api.ai['remove-background'].$post({ json: { image } });
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
+      return response.json();
+    },
+
+    onSuccess({ data }) {
+      addImage(data);
+    },
+  });
 
   useOnSelectionChange((objects) => setSelectedObject(objects[0]));
 
@@ -42,7 +79,16 @@ const RemoveBackgroundSidebar: React.FC = () => {
               <div className="relative aspect-square rounded-md overflow-hidden transition bg-muted">
                 <Image src={src} alt="Image" className="object-cover" fill />
               </div>
-              <Button className="w-full">Remove background</Button>
+              <Button
+                className="w-full"
+                disabled={mutation.isPending}
+                onClick={() => {
+                  mutation.mutate(src);
+                }}
+              >
+                {mutation.isPending && <Loader2 className="w-4 h-4 animate-spin" />}
+                Remove background
+              </Button>
             </div>
           </ScrollArea>
         )}
