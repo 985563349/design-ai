badc0023ee7997f004093ff28349d61927339123
王杰 <jie985563349@outlook.com>
2024-11-13 13:18:31 +0800

    add dockerfile

--- src/app/design/[slug]/page.tsx
+++ src/app/design/[slug]/page.tsx
@@ -8,7 +8,10 @@ import Editor from './components/editor';
 const fetchProject = async (id: string) => {
   const url = client.api.projects[':id'].$url({ param: { id } });
   const cookieStore = await cookies();
-  const response = await fetch(url, { cache: 'no-cache', headers: { cookie: cookieStore.toString() } });
+  const response = await fetch(url, {
+    cache: 'no-cache',
+    headers: { cookie: cookieStore.toString() },
+  });
 
   if (!response.ok) {
     throw new Error('Something went wrong');
@@ -17,7 +20,11 @@ const fetchProject = async (id: string) => {
   return response.json();
 };
 
-export default async function Design({ params }: { params: Promise<{ slug: string }> }) {
+export default async function Design({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
   const session = await auth();
 
